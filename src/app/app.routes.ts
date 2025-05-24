d71307dc836a6da9abe15f8bad0875b8714cb883
Vinicius Cesario <vini.cesario@gmail.com>
2025-05-24 20:00:43 -0300

    main and intial config for btn

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
-import { Routes } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
+import { Routes } from '@angular/router';;
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {
         path: "",
-        component: HeaderComponent
+        component: HomeComponent
     }
 ];
---
+++ src/app/components/btn-primary/btn-primary.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-primary',
+  imports: [],
+  templateUrl: './btn-primary.component.html',
+  styleUrl: './btn-primary.component.scss'
+})
+export class BtnPrimaryComponent {
+
+}
